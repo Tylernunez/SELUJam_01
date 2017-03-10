ff070ff76767a063bf39948b3f7c09208fbe6f1e
Tyler Nunez <tyler.nunez@selu.edu>
2017-03-10 14:17:53 -0600

    Added all kinds of cool stuff. Map movement is now possible, I have included new scenes and navigation through those scenes as well as additional functions for the player such as jump and IsGrounded.

---
+++ gameJam/Assets/scripts/playerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour {
+
+
+    public float thrust = 6.0f;
+    public Rigidbody rb;
+    private float distToGround;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        Collider col = this.GetComponent<Collider>();
+        distToGround = col.bounds.extents.y;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.Space) && IsGrounded())
+        {
+            rb.AddForce(new Vector3(0, 1 * thrust, 0), ForceMode.Impulse);
+            print("The space bar was pressed.");
+        }
+            
+    }
+    bool IsGrounded()
+    {
+        return Physics.Raycast(transform.position, -Vector3.up, distToGround + 0.1f);
+    }
+
+}
---
+++ gameJam/Assets/scripts/sceneManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class sceneManager : MonoBehaviour {
+
+
+	public void LoadScene(string destination)
+	{
+			SceneManager.LoadScene(destination);
+	}
+
+	public void setMainMenu()
+	{
+
+		LoadScene("mainmenu");
+	}
+
+	public void setExitGame()
+	{
+		Application.Quit();
+	}
+	public void setLevelSelect()
+	{
+
+		LoadScene("levelselect");
+	}
+	public void setLeveltest()
+	{
+
+		LoadScene("mapMoveTest");
+	}
+}
