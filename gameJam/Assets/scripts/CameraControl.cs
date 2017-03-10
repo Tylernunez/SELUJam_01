349ad1484af80e8269abe5e13fb6a1e846fc8471
James Ostarly <james.ostarly@selu.edu>
2017-03-10 14:18:02 -0600

    added a camera testing scene, as well as a camera controller; it currently has no rotation limits

---
+++ gameJam/Assets/scripts/CameraControl.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraControl : MonoBehaviour
+{
+    public float horizontalSpeed = 2.0F;
+    public float verticalSpeed = 2.0F;
+    public float lockPos = 0;
+
+    void Update()
+    {
+        float h = horizontalSpeed * Input.GetAxis("Horizontal");
+        float v = verticalSpeed * Input.GetAxis("Vertical");
+        transform.Rotate(v, h, 0);
+        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, lockPos);
+    }
+}
